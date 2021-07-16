71c35a1d57c983cac362fd88c9335f6deb0f1d67
Ciro Botelho Lacerda <cirolacerda@gmail.com>
2021-07-15 22:45:43 -0300

    Desafio implementado

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,43 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    
+    const newTask = {
+      id: Math.random(),
+      title: newTaskTitle,
+      isComplete: false
+    };
+
+    if(newTaskTitle) {
+
+      setTasks(oldState => [...oldState, newTask]);
+      setNewTaskTitle('');
+
+    }else {
+      alert("Digite um título");
+      return;
+    }
+
+        
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const tasksToggled = tasks.map( task => task.id === id ? {
+      ...task,
+      isComplete: !task.isComplete
+    }: task );
+
+    setTasks(tasksToggled);
+     
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const tasksRemoved = tasks.filter( task => task.id !== id);
+
+    setTasks(tasksRemoved);
+
   }
 
   return (
